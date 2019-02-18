7bab8da14321523ea33d3f31c1c5ef00ab1e052a
Alonso Bolarte <ugoalonso@hotmail.com>
2019-02-18 10:42:25 -0500

    Starting Canvas in Gameplay Scene for Battle

---
+++ Rogue-Project/Assets/DefaultHighlight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DefaultHighlight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(gameObject, null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
