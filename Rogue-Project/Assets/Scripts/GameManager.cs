8a039de159243251b967195039d91b61ab248a6b
Tetewet <theogorisse@gmail.com>
2019-02-25 08:34:47 -0500

    Auto stash before merge of "SPRINTenCOURS" and "origin/Chris"

--- Rogue-Project/Assets/Scripts/GameManager.cs
+++ Rogue-Project/Assets/Scripts/GameManager.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public enum GameState { PAUSE, GAMEPLAY, OPTIONS }
+public enum GameState { PAUSE, GAMEPLAY, OPTIONS, FIGHTING }
 
 
 public class GameManager : MonoBehaviour
 {
+    //Variable List.//
+    public bool isWalking = false;
+    public bool CanFight = false; //Prevent from entering a fight twice => enemyVicinity(); //
+    public bool isFighting = false;
+
+    public int enemyCount = 0;
+
+    //Lists.//
+    public List<GameObject> NumberOfEnemies = new List<GameObject>(); //Manages the numbers of enemies inside a Fight.//
+
+
+    public static GameManager gameManager;
+    public GameObject MainCharacter;
+
+    //Thingy to make battle screen appear, Brad's way thing.
+
     private AsyncOperation async;
     public void StartBtn(int i)
     {
@@ -20,6 +36,16 @@ public class GameManager : MonoBehaviour
     [Header("Game State")]
     public GameState currentState;
 
+    void Awake()
+    {
+        CheckGM();
+
+        if (!GameObject.Find("MainCharacter"))
+        {
+            GameObject MC = Instantiate(MainCharacter, Vector3.zero, Quaternion.identity) as GameObject; //Set the MC vector to 0 and same for rotation (Quaternion).//
+            MC.name = "Main Character";
+        }
+    }
 
     void Start()
     {
@@ -32,6 +58,62 @@ public class GameManager : MonoBehaviour
         if (Input.GetButtonDown("Cancel"))
         {
             pauseGame();
+        }
+        switch (currentState)
+        {
+            case (GameState.GAMEPLAY):
+
+                if (isWalking)
+                {
+                    vicinityEnemy();
+                }
+                if (isFighting)
+                {
+                    currentState = GameState.FIGHTING;
+                }
+
+                break;
+
+            case (GameState.FIGHTING):
+                //Make shit pop-up.//
+
+                break;
+        }
+    }
+
+    private void vicinityEnemy()
+    {
+        if (isWalking && CanFight) //Prevent from entering a fight twice.//
+        {
+            //Enemy is within range.//
+            isFighting = true;
+        }
+    }
+    private void StartFight()
+    {
+        //Count how many enemies are in the vicinity.//
+        for (int i = 0; i < enemyCount; i++)
+        {
+            //NumberOfEnemies.Add(  //Add enemies within range? // )
+
+        }
+
+        //Popup scene Brad's way.//
+
+        //Player Reset.//
+        isWalking = false;
+        isFighting = false;
+        CanFight = false;
+    }
+    private void CheckGM() //Check for gameManager object duplication.//
+    {
+        if (gameManager = this)//If the gameManager exists, if not set it to the GameObject.//
+        {
+            gameManager = this;
+        }
+        else if (gameManager != this) //If it already exists, destroy it to prevent duplicates.//
+        {
+            Destroy(gameObject);
         }
     }
 
@@ -58,7 +140,6 @@ public class GameManager : MonoBehaviour
         }
 
     }
-    
     public void optionsButton()
     {
         //PausePannel.SetActive(false);
@@ -71,7 +152,6 @@ public class GameManager : MonoBehaviour
         PausePannel.SetActive(true);
         changeState(GameState.PAUSE);
     }
-
     public void Exit()
     {
 #if UNITY_EDITOR        
