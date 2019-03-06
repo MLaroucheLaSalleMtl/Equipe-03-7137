ddb113a196afa7c5d5792bc21bcaa79be9365635
Tetewet <theogorisse@gmail.com>
2019-03-06 13:48:06 -0500

    break house
    break house sans le canvas car il est disparu

---
+++ Rogue-Project/Assets/BedScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedScript : MonoBehaviour
+{
+
+    public GameObject BedPannel;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && gameObject.tag == "BedCollider")
+        {
+            print("colliding with Bed");
+            bool pauseState = BedPannel.activeSelf;
+        }
+
+        if (collision.gameObject.tag == "Player" && gameObject.tag == "DoorCollider")
+        {
+            EntryManager.CabinExit(ref collision);
+        }
+    }
+
+    public void yesButton()
+    {
+        //il faut mettre le code pour remplir le HP
+        BedPannel.SetActive(false);
+    }
+    public void noButton()
+    {
+        //il faut mettre le code pour remplir le HP
+        BedPannel.SetActive(false);
+    }
+}
