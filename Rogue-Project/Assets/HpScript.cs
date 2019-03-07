0850c6a53994e4eca0f1eae9db8b01c07625608b
Alonso Bolarte <ugoalonso@hotmail.com>
2019-03-07 13:40:03 -0500

    MadeEverythingWork

---
+++ Rogue-Project/Assets/HpScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpScript : MonoBehaviour
+{
+    public Text text;
+    public PlayerBaseClass player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = (int)(player.currentHP / player.baseHP*100) + "%";
+    }
+}
