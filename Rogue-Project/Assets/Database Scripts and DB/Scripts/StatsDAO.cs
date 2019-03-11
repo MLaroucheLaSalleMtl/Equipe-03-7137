75a07f12597b622534b57d17ec301d0915edc34c
Unknown <hemondchristopher@gmail.com>
2019-03-11 10:16:02 -0400

    Database smol init

---
+++ Rogue-Project/Assets/Database Scripts and DB/Scripts/StatsDAO.cs
@@ -0,0 +1,33 @@
+﻿using Mono.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+public class StatsDAO
+{
+    void Awake()
+    {
+        StatsDAO.DatabasePath = "URI=file:" + Application.dataPath + "/Database Scripts and DB/Database/Stats.db";
+    }
+
+
+    //Insert > Values inside the db in order.//
+    public static void Insert(string name, int cost)
+    {
+        ExecuteCommand($"INSERT INTO Stats VALUES ('{name}', '{cost}')", reader => { });
+    }
+
+    public static void ExecuteCommand(string commandText, Action<IDataReader> dataHandler)
+    {
+        IDbConnection connection = new SqliteConnection(DatabasePath);
+        connection.Open();
+        IDbCommand command = connection.CreateCommand();
+        command.CommandText = commandText;
+        IDataReader reader = command.ExecuteReader();
+
+        dataHandler(reader);
+
+        reader.Close();
+        connection.Close();
+    }
+}
