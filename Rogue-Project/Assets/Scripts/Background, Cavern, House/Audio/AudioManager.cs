9297a10370f57924b5ab7a22fc06693c9e70f4a9
Unknown <hemondchristopher@gmail.com>
2019-04-16 21:49:17 -0400

    Working audio - Gotta fix Cave in and out battle

---
+++ Rogue-Project/Assets/Scripts/Background, Cavern, House/Audio/AudioManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Variables
+    public SoundClass[] sound;
+    public static AudioManager instance;
+    #endregion
+
+    #region Awake, Start
+    void Awake()
+    {
+        foreach(SoundClass sounds in sound)
+        {
+            sounds.source = gameObject.AddComponent<AudioSource>();
+            sounds.source.clip = sounds.audioClip ;
+
+            sounds.source.volume = sounds.volume;
+            sounds.source.pitch = sounds.pitch;
+            sounds.source.loop = sounds.loop;
+        }
+    }
+
+    void Start()
+    {
+      Play("OnGrass");
+    }
+    #endregion
+
+    #region Sound Functions
+    public void Play(string name)
+    {
+        SoundClass sounds = Array.Find(sound, checkSound => checkSound.name == name);
+
+        if(sounds == null)
+        {
+            Debug.LogWarning("Sound: " + name + " Not found!");
+            return;
+        }
+        sounds.source.Play();
+        
+    }
+    public void Stop()
+    {
+        foreach(SoundClass sounds in sound)
+        {
+            sounds.source.Stop();
+        }
+    }
+
+    public void switcharoo(string name)
+    {
+        FindObjectOfType<AudioManager>().Stop();
+        FindObjectOfType<AudioManager>().Play(name);
+    }
+    #endregion
+}
